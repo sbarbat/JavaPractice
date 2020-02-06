b19064a4725ef18ef7fb8b86bbe78866a7ea5c8a
Santi Barbat <barbatsan@gmail.com>
2020-02-06 12:46:03 +0000

    Threaded streams example

--- src/streams/ThreadedStreams.java
+++ src/streams/ThreadedStreams.java
@@ -1,5 +1,25 @@
 package streams;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ThreadedStreams {
 
+	private final static List<String> words =
+			Arrays.asList("This", "is", "my", "first", "streams", "test", "I", "am", "Santi", "Barbat");
+	
+	public static void main(String[] args) {
+		
+		// Paralell Stream
+		(new Thread(() -> {
+			words.parallelStream()
+				.forEach((s) -> { System.out.println(s); });
+		})).start();
+
+		// Async Stream
+		(new Thread(() -> {
+			words.stream()
+				.forEach((s) -> { System.out.println(s); });
+		})).start();
+	}
 }
